fe4759d835f1bf7bc42a67325389e17deb4bfb7c
taks-mohs <taks.compsci@gmail.com>
2024-11-19 11:29:22 -1000

    first commit

---
+++ screens/SecondPage.js
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from "react-native";
+
+function SecondPage(props) {
+  return (
+    <Text>Second Page</Text>
+  );
+}
+
+export default SecondPage;
+
+const styles = StyleSheet.create({});
