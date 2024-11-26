aec271b273672bbc77355524a973acbd3cf196a4
seanstaka <seanstaka@gmail.com>
2024-11-25 15:29:02 -1000

    style to main page 2

--- screens/SecondPage.js
+++ screens/SecondPage.js
@@ -2,10 +2,15 @@ import { StyleSheet, Text, View } from "react-native";
 
 function SecondPage(props) {
   return (
-    <Text>Modify Second Page</Text>
+    <Text style={styles.textStyle}>Modify Second Page</Text>
   );
 }
 
 export default SecondPage;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  textStyle : {
+    fontSize: 24,
+    fontWeight: 'bold'
+  }
+});
