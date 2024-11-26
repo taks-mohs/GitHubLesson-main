a2c2c7145ac191ed62c106bcfe4c3ba798f74146
taks-mohs <taks.compsci@gmail.com>
2024-11-25 15:19:59 -1000

    Styling third page

--- screens/ThirdPage.js
+++ screens/ThirdPage.js
@@ -1,11 +1,19 @@
 import { StyleSheet, Text, View } from "react-native";
 
 function ThirdPage(props) {
-  return (
-    <Text>Adding Third Page</Text>
-  );
+    return (
+        <View style={styles.container}>
+            <Text>Adding Third Page</Text>
+        </View>
+
+    );
 }
 
 export default ThirdPage;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#195392'
+    }
+});
