7e7addfcae2c17b9adcc6e5e8a9175bd79657943
seanstaka <seanstaka@gmail.com>
2025-01-26 19:59:16 -1000

    recent changes when opened on 1/26/25

--- App.js
+++ App.js
@@ -1,15 +1,16 @@
 import { StyleSheet, SafeAreaView } from 'react-native';
-import FirstPage from './screens.FirstPage.js';
+//import FirstPage from './screens.FirstPage.js';
 
 import { useState } from 'react';
 
 export default function App() {
   const [pageNum, setPageNum] = useState(0);
 
-  let screen = <FirstPage />
+  let screen = <Text>Screen Part</Text>//<FirstPage />
 
   return (
     <SafeAreaView style={styles.container}>
+      <Text>Hello World!</Text>
       {screen}
     </SafeAreaView>
   );
